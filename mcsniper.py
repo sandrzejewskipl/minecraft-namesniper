3b4c107495360ea67aa2ef37b7e857e7c262cc15
Szymon Andrzejewski <hello@szymonandrzejewski.pl>
2021-12-29 16:37:21 +0100

    update useragent

--- mcsniper.py
+++ mcsniper.py
@@ -7,7 +7,7 @@ import datetime as dt
 from datetime import datetime
     
 # User-Agent
-useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
+useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
 
 
 # Gui
